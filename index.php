ca64f31778a9b77588791f77c12c337ccfc1fedc
Chiaki Ujiie <ujiie.c615@gmail.com>
2021-11-27 22:10:32 +0900

    no message

---
+++ index.php
@@ -0,0 +1,178 @@
+<?php get_header(); ?>
+
+            <!-- slider -->
+        <section>
+            <div class="slider_wrap">
+                <?php echo do_shortcode('[smartslider3 slider="2"]');?>
+            </div>
+        </section>
+    <main>
+        <!-- content -->
+        <section>
+            <div class="section" id=section1>
+                <h1>園からのお知らせ</h1>
+                <div class="section_small">
+                    <p class="section_small_text">
+                        2020/12/01　9月献立表を更新しました。
+                    </p>
+                    <p class="section_small_text">
+                        2020/12/01　9月献立表を更新しました。
+                    </p>
+                    <p class="section_small_text">
+                        2020/12/01　9月献立表を更新しました。
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="section2" id=section2>
+                <h1>クッピー乳児園の想い</h1>
+                <div class="omoi_box">
+                    <div class="omoi_back1">
+                        <div class="omoi_wrap">
+                            <span class="omoi_no">１</span>
+                            <p class="omoi_text">子どもの幸せを1番に願い、心身ともに豊かでたくましい子どもの育成を目指します。</p>
+                        </div>
+                    </div>
+                    <div class="omoi_back1">
+                        <div class="omoi_wrap">
+                            <span class="omoi_no">２</span>
+                            <p class="omoi_text">乳幼児の大切な時期を家庭的な雰囲気の中で一人一人を手厚く保育します。</p>
+                        </div>
+                    </div>
+                    <div class="omoi_back1">
+                        <div class="omoi_wrap">
+                            <span class="omoi_no">３</span>
+                            <p class="omoi_text">健やかな心身の発達を第一に目指し、養護と教育が一体となった保育をします。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="section" id=section3>
+                <div class="section_line">
+                    <img src="/assets/images/line_flower.png" alt="">
+                </div>
+                <h1>小規模保育園とは？</h1>
+                <div class="section3_box">
+                    <div class="section3_square" id="square1">
+                        <img src="/assets/images/icon1.png" alt="">
+                        <p class="square">０〜３歳未満対象</p>
+                    </div>
+                    <div class="section3_square" id="square2">
+                        <img src="/assets/images/icon2.png" alt="">
+                        <p class="square">定員数19人以下の<br>
+                            小規模保育</p>
+                    </div>
+                    <div class="section3_square" id="square3">
+                        <img src="/assets/images/icon3.png" alt="">
+                        <p class="square">２歳児クラス卒園後、<br>
+                            認可保育園へ進級</p>
+                    </div>
+                </div>
+                
+                <p class="section3_text">クッピー乳児園では、通常の保育士の配置基準より<br>
+                    保育士を1名多く配置しており、<br>
+                    個々の発達に応じた丁寧な保育を行うことができます。</p>
+                
+                <div class="section_small">
+                    <h2>＜連携施設一覧＞</h2>
+                    <p class="section_small_text">
+                        ①どんぐりの里保育園　②赤道あおぞら保育園　<br>③ポケット保育園　④ぎのわんおひさま保育園
+                    </p>
+                </div>
+
+                </div>
+            </div>
+
+        </section>
+        <div class="section_line">
+            <img src="/assets/images/line_flower.png" alt="">
+        </div>
+        <section>
+            <div id="section4">
+                <h1>園について</h1>
+                <div class="section_table">
+                    <h2 class="h2_box">定員状況</h2>
+                    <p class="table_text">※現在空きはございません。（８月１日時点）</p>
+                    <table class="syousai_table">
+                        <tr>
+                            <th>年齢</th>
+                                <td>0歳児</td>
+                                <td>1歳児</td>
+                                <td>2歳児</td>
+                        </tr>
+                        <tr>
+                            <th>定員</th>
+                                <td>6名</td>
+                                <td>6名</td>
+                                <td>7名</td>
+                        </tr>
+                        <tr>
+                            <th>現員</th>
+                                <td>6名</td>
+                                <td>6名</td>
+                                <td>7名</td>
+                        </tr>
+                    </table>
+
+                </div>
+                <div class="section_table">
+                    <h2 class="h2_box">保育時間</h2>
+                    <h3 class="h3_left"><span class="table_subtitle">通常保育</span></h3>
+                        <table class="syousai_table">
+                            <tr>
+                                <th>保育時間</th>
+                                    <td class="td_left">7：00～18：00</td>
+                            </tr>
+                            <tr>
+                                <th>延長保育</th>
+                                <td class="td_left">18：00～19：00（利用料 300円 / 1日 2500円 / 月額）</td>
+                            </tr>
+                        </table>
+                    <h3 class="h3_left"><span class="table_subtitle">土曜保育</span></h3>
+                        <table class="syousai_table">
+                            <tr>
+                                <th>保育時間</th>
+                                <td class="td_left">7：00～18：00</td>
+                            </tr>
+                        </table>
+                </div>
+                <div class="section_table">
+                    <h2 class="h2_box">概要</h2>
+                    <table class="syousai_table">
+                        <tr>
+                            <th>事業所名</th>
+                            <td class="td_left">クッピー乳児園</td>
+                        </tr>
+                        <tr>
+                            <th>電話番号</th>
+                            <td class="td_left">098-898-5657</td>
+                        </tr>
+                        <tr>
+                            <th>住所</th>
+                            <td class="td_left">〒901-2214　沖縄県宜野湾市我如古1-55-13-2F</td>
+                        </tr>
+                        <tr>
+                            <th>開園日</th>
+                            <td class="td_left">月曜〜土曜</td>
+                        </tr>
+                        
+                    </table>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="section5">
+                <h1>アクセスマップ</h1>
+                <div class="map pc">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28624.863267331!2d127.757005!3d26.258161!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfdf388e35d023df7!2z44Kv44OD44OU44O85Lmz5YWQ5ZyS!5e0!3m2!1sja!2sjp!4v1637250734732!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>                </div>
+                <div class="map sp">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28624.863267331!2d127.757005!3d26.258161!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfdf388e35d023df7!2z44Kv44OD44OU44O85Lmz5YWQ5ZyS!5e0!3m2!1sja!2sjp!4v1637250734732!5m2!1sja!2sjp" width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>                </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+<?php get_footer(); ?> 
